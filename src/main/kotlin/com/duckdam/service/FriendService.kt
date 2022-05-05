002d5d64deaba7334a309be8c34d928648081d10
0JIEUN0 <je300512@gmail.com>
2022-05-05 22:31:05 +0900

    feat(friend-service): add support for find friends by uid

--- src/main/kotlin/com/duckdam/service/FriendService.kt
+++ src/main/kotlin/com/duckdam/service/FriendService.kt
@@ -2,8 +2,12 @@ package com.duckdam.service
 
 import com.duckdam.domain.friend.Friend
 import com.duckdam.domain.friend.FriendRepository
+import com.duckdam.domain.user.User
 import com.duckdam.domain.user.UserRepository
+import com.duckdam.dto.friend.FriendResponseDto
 import com.duckdam.errors.exception.NotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
@@ -18,5 +22,24 @@ class FriendService (
             throw NotFoundException("User [${targetId}] was not registered.")
         }
         friendRepository.save(Friend(uid = uid, friendId = targetId))
+    }
+
+    fun findMyFriends(uid: Long):ResponseEntity<List<FriendResponseDto>> {
+        val friendList: List<Friend> = friendRepository.findAllByUid(uid)
+        val friendResList: MutableList<FriendResponseDto> = mutableListOf()
+        friendList.forEach {
+            val user: User = userRepository.findById(it.friendId).get()
+            friendResList.add(
+                FriendResponseDto(
+                uid =  user.id,
+                name = user.name,
+                profile = user.profile
+            ))
+        }
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(
+                friendResList
+            )
     }
 }
--- src/test/kotlin/com/duckdam/service/FriendServiceTest.kt
+++ src/test/kotlin/com/duckdam/service/FriendServiceTest.kt
@@ -3,10 +3,11 @@ package com.duckdam.service
 import com.duckdam.MockDto
 import com.duckdam.domain.friend.FriendRepository
 import com.duckdam.domain.user.UserRepository
+import com.duckdam.dto.friend.FriendResponseDto
 import com.duckdam.dto.user.RegisterDto
-import com.duckdam.errors.exception.ConflictException
 import com.duckdam.errors.exception.NotFoundException
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -75,4 +76,34 @@ class FriendServiceTest {
         }
     }
 
+    @Test
+    fun is_findMyFriends_works_well_on_empty() {
+        // arrange
+        val uid1: Long = userService.register(mockRegisterDto)
+
+        // act
+        val friendList: List<FriendResponseDto> = friendService.findMyFriends(uid1).body!!
+
+        // assert
+        assertThat(friendList.size).isEqualTo(0)
+    }
+
+    @Test
+    fun is_findMyFriends_works_well() {
+        // arrange
+        val uid1: Long = userService.register(mockRegisterDto)
+        val uid2: Long = userService.register(mockRegisterDto.copy(email = "email", name="name"))
+        val uid3: Long = userService.register(mockRegisterDto.copy(email = "email2", profile = "test"))
+
+        friendService.followFriend(uid1, uid2)
+        friendService.followFriend(uid1, uid3)
+
+        // act
+        val friendList: List<FriendResponseDto> = friendService.findMyFriends(uid1).body!!
+
+        // assert
+        assertThat(friendList.size).isEqualTo(2)
+        assertThat(friendList[0].name).isEqualTo("name")
+        assertThat(friendList[1].profile).isEqualTo("test")
+    }
 }
